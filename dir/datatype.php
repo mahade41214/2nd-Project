b06974cd3d47190f351118adf7f4ce210f0c8656
mahade41214 <mahade41214@gmail.com>
2024-12-14 11:21:59 +0600

    2nd Project

---
+++ dir/datatype.php
@@ -0,0 +1,65 @@
+<?php
+$name = "Mahade";
+$name = 500;
+
+/* php data types:
+1. String
+2.Integer
+3.Float
+4. Boolean
+5. Object
+6. Array
+7. NULL */
+
+//String - Sequence of Characters
+
+$name = "John";
+$friend = "Robin";
+ echo "$name ka Freind is $friend";
+
+ //Integer - None Decimal Number
+
+ $income = 655;
+ $debts = -500;
+ echo "<br>";
+ echo $income;
+ echo "<br>";
+ echo $debts;
+
+ // Float - Decimal point number
+
+ $income = 655.3;
+ $debts = -500.8;
+ echo "<br>";
+ echo $income;
+ echo "<br>";
+ echo $debts;
+ echo "<br>";
+
+ // Boolean - Can be either true or false
+$x = true;
+$is_friend = false;
+echo var_dump($x);
+echo "<br>";
+echo var_dump($is_friend);
+echo "<br>";
+
+// Object - Instances of classes
+// Employee is a class ---> Mahade can be one object
+
+
+//Array - use to store multiple values in a single variable
+$friends = array("rohan", "shuhan", "skillf", "Larry");
+echo var_dump($friends);
+echo "<br>";
+echo $friends[0];
+echo "<br>";
+echo $friends[1];
+echo "<br>";
+echo $friends[2];
+echo "<br>";
+echo $friends[3];
+echo "<br>";
+echo "vedio no 08 will be run";
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+ <?php
+
+function will_add($value1,$value2){
+    return $value1+$value2;
+}
+$add=will_add(15,5);
+//echo $add;
+
+
+/*if($add<12){
+echo " welcome";
+
+}else{
+    echo "go back";
+}*/
+
+
+ ?>
+
+
